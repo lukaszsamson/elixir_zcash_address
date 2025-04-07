3c803c6950ab6030de3537b5819ef025656d9e3f
Lukasz Samson <lukaszsamson@gmail.com>
2025-04-07 10:56:06 +0200

    update blake2_elixir

--- mix.exs
+++ mix.exs
@@ -21,11 +21,7 @@ defmodule ZcashAddress.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:blake2_elixir, "~> 0.8"}
-      # https://github.com/riverrun/blake2_elixir/pull/2
-      {:blake2_elixir, github: "josevalim/blake2_elixir", branch: "patch-1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:blake2_elixir, "~> 0.8"}
     ]
   end
 end
